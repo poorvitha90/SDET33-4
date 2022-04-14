1706ab222f05d077d0ec0b4ab22ec91a7b81550f
Prakruthi <Prakruthi@DESKTOP-0GKRM8F>
2022-04-14 18:10:35 +0530

    class2 parameterized

--- src/test/java/com/jenkin/practice/Class2Test.java
+++ src/test/java/com/jenkin/practice/Class2Test.java
@@ -6,6 +6,10 @@ public class Class2Test {
 	@Test
 	public void class2Test()
 	{
+		String browser = System.getProperty("Browser");
+		System.out.println(browser);
+		String url = System.getProperty("Url");
+		System.out.println(url);
 		System.out.println("class2 running");
 		System.out.println("class2 again running");
 		//commit1
