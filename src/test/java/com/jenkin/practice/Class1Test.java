63f496c8dd44a00968b33b7feb1640737fa535e2
Prakruthi <Prakruthi@DESKTOP-0GKRM8F>
2022-04-14 13:14:17 +0530

    first commit

---
+++ src/test/java/com/jenkin/practice/Class1Test.java
@@ -0,0 +1,12 @@
+package com.jenkin.practice;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	@Test
+	public void class1Test()
+	{
+		System.out.println("class1 running");
+	}
+
+}
---
+++ src/test/java/com/jenkin/practice/Class2Test.java
@@ -0,0 +1,12 @@
+package com.jenkin.practice;
+
+import org.testng.annotations.Test;
+
+public class Class2Test {
+	@Test
+	public void class2Test()
+	{
+		System.out.println("class2 running");
+	}
+
+}
